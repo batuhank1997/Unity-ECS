c6bff4233391af1f013323297ed05600207b3418
Batuhan <batuhank1997@hotmail.com>
2023-03-28 22:40:01 +0300

    project reset/removed all files

--- Assets/Scripts/Components/HealthComponent.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using Entitas;
-using UnityEngine;
-
-public sealed class HealthComponent : IComponent
-{
-    public float value;
-}
--- Assets/Scripts/Components/PositionComponent.cs
+++
@@ -1,7 +0,0 @@
-﻿using Entitas;
-using UnityEngine;
-
-public sealed class PositionComponent : IComponent
-{
-    public Vector3 value;
-}
